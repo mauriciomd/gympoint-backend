36dbc577b1cf3e2f1474eb6aad014ea11b41f021
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-23 11:50:04 -0300

    feat: add MembershipRepository implementation using typeorm

---
+++ src/modules/memberships/infra/typeorm/repositories/MembershipRepository.ts
@@ -0,0 +1,40 @@
+import { Repository, getRepository } from 'typeorm';
+
+import IMembershipRepository from '../../../repositories/IMembershipRepository';
+import { ICreateMembershipDTO } from '../../../dtos/ICreateMembershipDTO';
+import Membership from '../entities/Membership';
+
+class MembershipRepository implements IMembershipRepository {
+  private membershipRepository: Repository<Membership>;
+
+  constructor() {
+    this.membershipRepository = getRepository(Membership);
+  }
+
+  public async create({
+    title,
+    duration,
+    price,
+  }: ICreateMembershipDTO): Promise<Membership> {
+    const newMembership = this.membershipRepository.create({
+      title,
+      duration,
+      price,
+    });
+
+    await this.membershipRepository.save(newMembership);
+    return newMembership;
+  }
+
+  public async findByTile(title: string): Promise<Membership | undefined> {
+    const membership = await this.membershipRepository.findOne({
+      where: {
+        title,
+      },
+    });
+
+    return membership;
+  }
+}
+
+export default MembershipRepository;
