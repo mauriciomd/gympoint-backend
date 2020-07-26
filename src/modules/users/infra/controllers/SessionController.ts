573deae539699bb6f633047e275364810f9d834c
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-26 16:50:07 -0300

    chore: add try/catch

--- src/modules/users/infra/controllers/SessionController.ts
+++ src/modules/users/infra/controllers/SessionController.ts
@@ -1,19 +1,38 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { container } from 'tsyringe';
 
 import CreateSessionService from '../../services/CreateSessionService';
+import HttpRequestError from '../../../../shared/errors/HttpRequestError';
 
 class SessionController {
-  public async create(request: Request, response: Response): Promise<Response> {
+  public async create(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Promise<Response | undefined> {
     const { email, password } = request.body;
 
-    const service = container.resolve(CreateSessionService);
-    const session = await service.execute({
-      email,
-      password,
-    });
+    try {
+      if (!email) {
+        throw new HttpRequestError('You have to inform an email');
+      }
 
-    return response.json(session);
+      if (!password) {
+        throw new HttpRequestError('You have to inform an password');
+      }
+
+      const service = container.resolve(CreateSessionService);
+      const session = await service.execute({
+        email,
+        password,
+      });
+
+      return response.json(session);
+    } catch (err) {
+      next(err);
+    }
+
+    return undefined;
   }
 }
 
