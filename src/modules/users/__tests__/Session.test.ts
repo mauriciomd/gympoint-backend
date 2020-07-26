f60f87e274c37da45c26ba1d3099102f4f3f2b30
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-26 16:49:48 -0300

    test: ensure that is not possible to create a session without email and password

--- src/modules/users/__tests__/Session.test.ts
+++ src/modules/users/__tests__/Session.test.ts
@@ -6,7 +6,7 @@ import app from '../../../shared/infra/http/app';
 let connection: Connection;
 describe('/sessions', () => {
   beforeAll(async () => {
-    connection = await createConnection('tests');
+    connection = await createConnection();
     await connection.runMigrations();
   });
 
@@ -26,5 +26,23 @@ describe('/sessions', () => {
 
     expect(response.body).toHaveProperty('token');
     expect(response.body).toHaveProperty('user');
+  });
+
+  it('should not be able to create a new session without email', async () => {
+    const response = await request(app).post('/sessions').send({
+      password: '123456',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  it('should not be able to create a new session without password', async () => {
+    const response = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
   });
 });
