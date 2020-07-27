f16b430e777c2e1cb70873de6802d50a60213646
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-27 16:34:30 -0300

    feat: register IStudentRepostory in dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,9 +6,17 @@ import UserRepository from '../../modules/users/infra/typeorm/repositories/UserR
 import IMembershipRepository from '../../modules/memberships/repositories/IMembershipRepository';
 import MembershipRepository from '../../modules/memberships/infra/typeorm/repositories/MembershipRepository';
 
+import IStudentRepository from '../../modules/students/repositories/IStudentRepository';
+import StudentRepository from '../../modules/students/infra/typeorm/repositories/StudentRepository';
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
 container.registerSingleton<IMembershipRepository>(
   'MembershipRepository',
   MembershipRepository,
+);
+
+container.registerSingleton<IStudentRepository>(
+  'StudentRepository',
+  StudentRepository,
 );
