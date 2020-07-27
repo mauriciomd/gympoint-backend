f14b85aa17d7ec004861f56efdbedb4a963f34a5
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-27 16:42:44 -0300

    feat: add fake student repository to allow tests

--- src/modules/students/repositories/fakes/FakeStudentRepository.ts
+++ src/modules/students/repositories/fakes/FakeStudentRepository.ts
@@ -1,17 +1,22 @@
+import { uuid } from 'uuidv4';
+
 import { ICreateStudentDTO } from '../../dtos/ICreateStudentDTO';
 import IStudentRepository from '../IStudentRepository';
 import Student from '../../infra/typeorm/entities/Student';
 
-class FakeUserRepository implements IStudentRepository {
+class FakeStudentRepository implements IStudentRepository {
   private storedStudents: Student[] = [];
 
   public async create(data: ICreateStudentDTO): Promise<Student> {
     const student = new Student();
-    Object.assign(student, data);
+    Object.assign(student, {
+      id: uuid(),
+      ...data,
+    });
 
     this.storedStudents.push(student);
     return student;
   }
 }
 
-export default FakeUserRepository;
+export default FakeStudentRepository;
