0d3e76d62580931a2c25a8d38ed05e7612a2edf6
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-29 16:37:45 -0300

    test: ensure that is possible to create enrollments

---
+++ src/modules/enrollments/__tests__/Enrollments.spec.ts
@@ -0,0 +1,132 @@
+import request from 'supertest';
+import { createConnection, Connection } from 'typeorm';
+
+import app from '../../../shared/infra/http/app';
+
+let connection: Connection;
+describe('/enrollments', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM enrollments');
+  });
+
+  afterAll(async () => {
+    await connection.query('DROP TABLE IF EXISTS memberships');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+    await connection.query('DROP TABLE IF EXISTS students');
+    await connection.query('DROP TABLE IF EXISTS enrollments');
+
+    await connection.close();
+  });
+
+  it('should not be able to create a new enrollment without a valid auth token', async () => {
+    const response = await request(app).post('/enrollments').send({
+      membershipId: 'valid-membership-id',
+      studentId: 'valid-student-id',
+      startDate: new Date(),
+    });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  it('should not be able to create a new enrollment enrollment a membershipId', async () => {
+    const user = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        studentId: 'valid-student-id',
+        startDate: new Date(),
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  it('should not be able to create a new enrollment without a studentId', async () => {
+    const user = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        membershipId: 'valid-membership-id',
+        startDate: new Date(),
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  it('should not be able to create a new enrollment without a start date', async () => {
+    const user = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        membershipId: 'valid-membership-id',
+        studentId: 'valid-student-id',
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  it('should be able to create a new enrollment', async () => {
+    const user = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+      password: '123456',
+    });
+
+    const membership = await request(app)
+      .post('/memberships')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        title: 'valid title',
+        price: 59,
+        duration: 12,
+      });
+
+    const student = await request(app)
+      .post('/students')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        name: 'Student Test',
+        email: 'student@test.com',
+        age: 33,
+        height: 190,
+        weight: 110,
+      });
+
+    const response = await request(app)
+      .post('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        membershipId: membership.body.id,
+        studentId: student.body.id,
+        startDate: new Date(),
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('total');
+    expect(response.body).toHaveProperty('endDate');
+  });
+});
