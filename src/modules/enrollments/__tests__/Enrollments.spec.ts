b87044c43effd08404537bbebf8de6224e4fa655
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-07-31 21:01:25 -0300

    feat: allow to delete an enrollment

--- src/modules/enrollments/__tests__/Enrollments.spec.ts
+++ src/modules/enrollments/__tests__/Enrollments.spec.ts
@@ -158,7 +158,7 @@ describe('/enrollments', () => {
         weight: 110,
       });
 
-    const x = await request(app)
+    await request(app)
       .post('/enrollments')
       .set('Authorization', `bearer ${user.body.token}`)
       .send({
@@ -174,5 +174,60 @@ describe('/enrollments', () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveLength(1);
     expect(response.body[0].isActive).toBe(true);
+  });
+
+  it('should be able to delete all the enrollments', async () => {
+    const user = await request(app).post('/sessions').send({
+      email: 'admin@gympoint.com',
+      password: '123456',
+    });
+
+    const membership = await request(app)
+      .post('/memberships')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        title: 'valid title',
+        price: 59,
+        duration: 12,
+      });
+
+    const student = await request(app)
+      .post('/students')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        name: 'Student Test',
+        email: 'student@test.com',
+        age: 33,
+        height: 190,
+        weight: 110,
+      });
+
+    const enrollment = await request(app)
+      .post('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`)
+      .send({
+        membershipId: membership.body.id,
+        studentId: student.body.id,
+        startDate: new Date(),
+      });
+
+    const enrollmentList = await request(app)
+      .get('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`);
+
+    expect(enrollmentList.status).toBe(200);
+    expect(enrollmentList.body).toHaveLength(1);
+
+    const deleteResponse = await request(app)
+      .delete(`/enrollments/${enrollment.body.id}`)
+      .set('Authorization', `bearer ${user.body.token}`);
+
+    const afterRemovingEnrollmentList = await request(app)
+      .get('/enrollments')
+      .set('Authorization', `bearer ${user.body.token}`);
+
+    expect(deleteResponse.status).toBe(200);
+    expect(afterRemovingEnrollmentList.status).toBe(200);
+    expect(afterRemovingEnrollmentList.body).toHaveLength(0);
   });
 });
