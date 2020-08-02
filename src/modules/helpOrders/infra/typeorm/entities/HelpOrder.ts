39c2fc74d722feaa8a9c534a6c3db5ced884239a
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-08-02 15:08:12 -0300

    feat: add help orders model

---
+++ src/modules/helpOrders/infra/typeorm/entities/HelpOrder.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Student from '../../../../students/infra/typeorm/entities/Student';
+
+@Entity('helpOrders')
+class HelpOrder {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  studentId!: string;
+
+  @ManyToOne(() => Student, student => student.id, { eager: true })
+  @JoinColumn({ name: 'studentId', referencedColumnName: 'id' })
+  student!: Student;
+
+  @Column()
+  question!: string;
+
+  @Column()
+  answer!: string;
+
+  @Column()
+  answeredAt!: Date;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
+
+export default HelpOrder;
---
+++ src/shared/infra/typeorm/migrations/1596390794483-CreateHelpOrderTableTable.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+class CreateHelpOrderTableTable1596390794483 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'helpOrders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+            isPrimary: true,
+          },
+          {
+            name: 'studentId',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'question',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'answer',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'answeredAt',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'helpOrderStudents',
+            columnNames: ['studentId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'students',
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('helpOrders');
+  }
+}
+
+export default CreateHelpOrderTableTable1596390794483;
